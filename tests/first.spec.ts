63b59390ec10ccfb284b4b6e0afbf01fe471414a
Saradha Mohan <saradha@quarksek.com>
2026-08-03 20:08:48 +0530

    Removed the console and added space in import

--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -1,8 +1,7 @@
-import{test,expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('Launch Application', async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
-  console.log("The page is launched successfully");
   await expect(page).toHaveTitle("Swag Labs");
   await expect(page).toHaveURL("https://www.saucedemo.com/");
 });
