c34ae89babe3056786a98ab52118953513592fe5
Saradha Mohan <saradha@quarksek.com>
2026-08-11 19:32:52 +0530

    commit changes in the login and locked files with the page object changes

---
+++ tests/locked_out.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+test('Invalid login', async ({ page }) => {
+     const loginPage = new LoginPage(page);
+        await loginPage.navigateTo();
+        await loginPage.login('locked_out_user', 'secret_sauce');
+    await expect(loginPage.errorMessage).toBeVisible();
+    await expect(loginPage.errorMessage).toContainText("Sorry");
+})
+
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,8 @@
-import { test, expect } from '@playwright/test';
-test('Invalid login', async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await page.getByPlaceholder('Username').fill("locked_out_user");
-    await page.getByPlaceholder('Password').fill("secret_sauce");
-    await page.getByRole('button', { name: 'Login' }).click();
-    const error_Message =await page.getByTestId('error')
-    await expect(error_Message).toBeVisible();
-})
+import {test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+test('User logs in', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateTo();
+    await loginPage.login('standard_user', 'secret_sauce');
+});
 
