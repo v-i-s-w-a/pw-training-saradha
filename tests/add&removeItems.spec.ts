bba17269ca887b48225ff4fcfc79c3f0144338a5
Saradha Mohan <saradha@quarksek.com>
2026-08-13 18:46:22 +0530

    commited the assert statements for add&removing items in the cart

--- tests/add&removeItems.spec.ts
+++ tests/add&removeItems.spec.ts
@@ -16,8 +16,11 @@ test('Add and Remove Items from Cart', async ({ page }) => {
    const itemNames = await cartPage.itemNames();
     console.log("Items in cart:", itemNames);
     expect(itemNames).toContain('Sauce Labs Backpack');
+    expect(itemNames).toContain('Sauce Labs Bike Light');
     await cartPage.removeItem('Sauce Labs Backpack');
     const updatedItemNames = await cartPage.itemNames();
     console.log("Items in cart after removal:", updatedItemNames);
+    expect(updatedItemNames).toContain('Sauce Labs Bike Light');
     expect(updatedItemNames).not.toContain('Sauce Labs Backpack');
+   
 });
