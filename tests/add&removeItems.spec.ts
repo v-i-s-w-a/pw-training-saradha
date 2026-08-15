b5d24abbb8cd865edb0d785f1b66c82fabccdea1
Saradha Mohan <saradha@quarksek.com>
2026-08-15 10:21:53 +0530

    commit change in add&remove spec files

--- tests/add&removeItems.spec.ts
+++ tests/add&removeItems.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { CartPage } from '../pages/CartPage';
+import { InventoryPage } from '../pages/InventoryPage';
 
 test('Add and Remove Items from Cart', async ({ page }) => {
     const loginPage = new LoginPage(page);
@@ -8,12 +9,12 @@ test('Add and Remove Items from Cart', async ({ page }) => {
     await loginPage.login('standard_user', 'secret_sauce');
     expect(page.url()).toContain('/inventory.html');
 
+    const inventoryPage = new InventoryPage(page);
+    await inventoryPage.addToCart('Sauce Labs Backpack');
+    await inventoryPage.addToCart('Sauce Labs Bike Light');
+    await inventoryPage.navigateTo();
     const cartPage = new CartPage(page);
-    await cartPage.addToCart('Sauce Labs Backpack');
-    await cartPage.addToCart('Sauce Labs Bike Light');
-    await cartPage.navigateTo();
-
-   const itemNames = await cartPage.itemNames();
+    const itemNames = await cartPage.itemNames();
     console.log("Items in cart:", itemNames);
     expect(itemNames).toContain('Sauce Labs Backpack');
     await cartPage.removeItem('Sauce Labs Backpack');
