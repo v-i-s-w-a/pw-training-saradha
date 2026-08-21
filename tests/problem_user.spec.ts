e8f8b33577da2be75fcee32871a280d8764eab71
Saradha Mohan <saradha@quarksek.com>
2026-08-21 19:44:49 +0530

    commit change in the problem_user spec file

---
+++ tests/problem_user.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Problem user sees the same image for all products', async ({ page }) => {
+    
+    await page.goto('https://www.saucedemo.com/inventory.html');
+    const imageSources = await page
+        .locator('.inventory_item_img img')
+        .evaluateAll(images =>
+            images.map(image => image.getAttribute('src'))
+        );
+        console.log(imageSources);
+      
+   expect(imageSources).toHaveLength(6);
+    expect(new Set(imageSources).size).toBe(1);
+});
