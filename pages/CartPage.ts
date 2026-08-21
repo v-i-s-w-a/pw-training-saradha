096352f714a367453263fbdabd88170198fb3bac
Saradha Mohan <saradha@quarksek.com>
2026-08-21 19:40:42 +0530

    Removed the itemNames() static menthod

--- pages/CartPage.ts
+++ pages/CartPage.ts
@@ -2,9 +2,6 @@ import { BasePage } from "./BasePage";
 import type { Page } from "@playwright/test";
 
 export class CartPage extends BasePage {
-    static itemNames() {
-        throw new Error('Method not implemented.');
-    }
 
     constructor(page: Page) {
         super(page, '/cart.html');
